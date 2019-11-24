88822cd5b9e04f4ff784e94a1747e7bb2065aa1a
Austin Hurst <mynameisaustinhurst@gmail.com>
2019-11-23 22:30:08 -0500

    Swapped Appveyor for Travis for Mac builds

--- build_mac.sh
+++ build_mac.sh
@@ -13,4 +13,4 @@ mkdir diskimage
 cp README diskimage/ReadMe.txt
 cp COPYING diskimage/License.txt
 cp -R XCode/SDL2_gfx/build/Release/SDL2_gfx.framework diskimage/
-hdiutil create -volname SDL2_gfx -srcfolder diskimage -ov -format UDZO ../${PWD##*/}.dmg
+hdiutil create -volname SDL2_gfx -srcfolder diskimage -ov -format UDZO ${PWD##*/}.dmg
